eb8c871068594da2dbb29af63fc80f1187b9d4f1
marieard <marie.ardisson@edu.escp.eu>
2020-11-23 09:59:44 +0100

    links and javascript add

---
+++ script.js
@@ -0,0 +1,15 @@
+function signUp() {
+  var elmnt = document.getElementById("contactUs");
+  elmnt.scrollIntoView({behavior: "smooth"});
+}
+
+
+function aboutUs() {
+  var elmnt = document.getElementById("aboutUs");
+  elmnt.scrollIntoView({behavior: "smooth"});
+}
+
+function topPage() {
+  var elmnt = document.getElementById("topPage");
+  elmnt.scrollIntoView({behavior: "smooth"});
+}
