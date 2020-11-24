a4132db6e702d63285130e4388a930780525707a
marieard <marie.ardisson@edu.escp.eu>
2020-11-24 13:59:20 +0100

    all links finished

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function signUp() {
   elmnt.scrollIntoView({behavior: "smooth"});
 }
 
-
 function aboutUs() {
   var elmnt = document.getElementById("aboutUs");
   elmnt.scrollIntoView({behavior: "smooth"});
@@ -11,5 +10,25 @@ function aboutUs() {
 
 function topPage() {
   var elmnt = document.getElementById("topPage");
+  elmnt.scrollIntoView({behavior: "smooth"});
+}
+
+function projectSummary() {
+  var elmnt = document.getElementById("project");
+  elmnt.scrollIntoView({behavior: "smooth"});
+}
+
+function projectKit() {
+  var elmnt = document.getElementById("kit");
+  elmnt.scrollIntoView({behavior: "smooth"});
+}
+
+function projectOnline() {
+  var elmnt = document.getElementById("online");
+  elmnt.scrollIntoView({behavior: "smooth"});
+}
+
+function projectHotLine() {
+  var elmnt = document.getElementById("hotline");
   elmnt.scrollIntoView({behavior: "smooth"});
 }
